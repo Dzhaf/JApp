d36a9496352d43bf29717fbd753d4eae4d55e70d
Dzhafar <iamdzhafar@gmail.com>
2022-06-25 16:20:25 +0300

    test: Add unit tests for CurrencyFormatter

---
+++ JAppUnitTests/CurrencyFormatterTests.swift
@@ -0,0 +1,41 @@
+//
+//  CurrencyFormatterTests.swift
+//  JAppUnitTests
+//
+//  Created by Jafar on 25.06.2022.
+//
+
+import Foundation
+import XCTest
+
+@testable import JApp
+
+class Test: XCTestCase {
+    
+    var formatter: CurrencyFormatter!
+    
+    override func setUp() {
+        super.setUp()
+        formatter = CurrencyFormatter()
+    }
+    
+    func testBreakDollarsIntoCents() throws {
+        let result = formatter.breakIntoDollarsAndCents(929466.23)
+        XCTAssertEqual(result.0, "929,466")
+        XCTAssertEqual(result.1, "23")
+
+    }
+    
+    func testDollarsFormated() throws {
+        let result = formatter.dollarsFormatted(929466.23)
+        XCTAssertEqual(result, "¤929,466.23")
+
+    }
+    
+    func testZeroDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(0.00)
+        XCTAssertEqual(result, "¤0.00")
+
+        
+    }
+}
